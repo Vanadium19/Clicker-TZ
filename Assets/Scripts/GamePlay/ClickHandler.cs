450fc60f39a30c49c607a72f9b36ddcf0a58a4a9
Vladislav <GimliVlad@list.ru>
2024-08-09 17:24:02 +0300

    Update click handler

--- Assets/Scripts/GamePlay/ClickHandler.cs
+++ Assets/Scripts/GamePlay/ClickHandler.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Zenject;
 
+[RequireComponent(typeof(ClickEffectsPool))]
 public class ClickHandler : MonoBehaviour, IPointerClickHandler
 {
     private IClickValue _clickValue;
     private ICurrencyAdder _currencyAdder;
+    private ClickEffectsPool _pool;
+
+    private void Awake()
+    {
+        _pool = GetComponent<ClickEffectsPool>();
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         _currencyAdder.AddCurrency(_clickValue.Value);
+        _pool.Pull().Play(_clickValue.Value);
     }
 
     [Inject]
